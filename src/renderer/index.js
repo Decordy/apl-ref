35660f63feac510f88c107e299a4e1c1a9b0f6ec
applearon <aronseshan@gmail.com>
2026-08-24 00:01:22 -0400

    Remove refresh room list button + automatically check for the room list

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,7 +1,7 @@
 // ── Theme Toggle ────────────────────────────────────────────────────────────
 
 import { User, Event, EventQueue, Room } from "./models.js"
-import { idFromUsername, osu, logEvent, MODS, confirmUI, log } from "./utils.js"
+import { idFromUsername, osu, logEvent, MODS, confirmUI, log, refreshRoomList } from "./utils.js"
 
 window.console.error = (...args) => {
     log.error(args.join(', '))
@@ -34,7 +34,7 @@ window.beatmaps = {}; // global cause like
 // i cant imagine that will cause problems?
 
 let rooms = {};
-let room_ids = []
+let room_ids = [] // TODO: unused? moved to utils.js
 let room;
 let connected = false;
 let countdown_id;
@@ -299,6 +299,7 @@ function hideRoomCreation() {
 
 function debugMode() { // this is kinda useless now but wtvs
     document.getElementById('room-setup').classList.remove('hidden')
+    document.getElementById('refresh-room-list').classlist.remove('hidden')
     const ping = document.getElementById("debug-menu")
     ping.classList.add('visible')
     document.getElementById('navbar-room-controls').classList.add('visible')
@@ -419,14 +420,7 @@ document.getElementById('home-button').addEventListener('click', async () => {
 
 
 // Refresh Room List
-document.getElementById('refresh-room-list').addEventListener('click', async () => {
-    const rooms_data = await osu.ListRooms()
-    room_ids = rooms_data.data.room_ids
-    document.getElementById("tabs").innerHTML = ""
-    for (let id of room_ids) {
-        addTab(id)
-    }
-})
+document.getElementById('refresh-room-list').addEventListener('click', refreshRoomList)
 
 // tab list
 function addTab(room_id) {
@@ -611,7 +605,7 @@ async function updateStatus() {
     let is_connected = status.data ? status.data.connected : false
     if (is_connected && !connected) {
         console.log(is_connected,connected, status)
-        osu.ListRooms()
+        refreshRoomList()
     }
     connected = is_connected;
     statusDot.classList.toggle('connected', connected)
@@ -634,6 +628,7 @@ document.getElementById('make-room-btn').addEventListener('click', async () => {
         room.queue = new EventQueue(room)
         hideRoomCreation()
         room.updateUI()
+        refreshRoomList()
     }
 })
 
--- src/renderer/models.js
+++ src/renderer/models.js
@@ -1,4 +1,4 @@
-import { idFromUsername, osu, GetBeatmap, MODS, showToast, confirmUI } from './utils.js'
+import { idFromUsername, osu, GetBeatmap, MODS, showToast, confirmUI, refreshRoomList } from './utils.js'
 
 function hideRoomActions() {
     document.getElementById('room-actions').classList.add('hidden')
@@ -222,6 +222,7 @@ export class Room {
         document.querySelector(`[class~="${playlist_id}"]`).querySelector('.playlist-item-id').textContent = beatmap.beatmapset.title + ` [${beatmap.version}]`
     }
     #addModSettingUI(mod_list, mod, mod_template) {
+        this.updateMode()
         let empty = true
         const mod_clone = mod_template.content.cloneNode(true);
         const settings_div = mod_clone.querySelector(".mod-item")
@@ -373,6 +374,7 @@ export class Room {
         document.getElementById('room-setup').classList.remove('hidden')
 
         document.getElementById("chat-messages").innerHTML = '<div id="no-messages" class="text-gray-500 dark:text-gray-400 text-sm italic">No messages yet...</div>'
+        refreshRoomList()
     }
 }
 
